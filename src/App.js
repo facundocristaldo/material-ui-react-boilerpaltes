6229bd68d7ed39f62e2ba73d043070e0c409e3ac
facundocristaldo <40257119+facundocristaldo@users.noreply.github.com>
2021-03-06 17:52:49 -0300

    themeprovider (#5)

--- src/App.js
+++ src/App.js
@@ -5,21 +5,33 @@ import Buttons from './components/Buttons'
 import Checkboxes from './components/Checkboxes';
 import Inputs from './components/Inputs';
 import StyledButtons from './components/StyledButtons';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+import { blue, orange } from '@material-ui/core/colors'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: orange[500]
+    },
+    secondary: {
+      main: blue[800]
+    }
+  }
+})
 
 function App() {
-  const style = {
-    fontSize: "1.2em"
-  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <Buttons />
-        <Checkboxes />
-        <Inputs />
-        <StyledButtons />
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+
+      <div className="App">
+        <header className="App-header">
+          <Buttons />
+          <Checkboxes />
+          <Inputs />
+          <StyledButtons />
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
