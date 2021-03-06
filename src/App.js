93ed40737ce2629c70517f2c629739b9be158691
Facundo Cristaldo <facu.cristaldo.96@gmail.com>
2021-03-06 18:05:34 -0300

    typography and some cleaning

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 //MaterialUI Comopnents
 import Buttons from './components/Buttons'
 import Checkboxes from './components/Checkboxes';
 import Inputs from './components/Inputs';
 import StyledButtons from './components/StyledButtons';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+import { ThemeProvider, createMuiTheme, Typography } from '@material-ui/core';
 import { blue, orange } from '@material-ui/core/colors'
+import 'fontsource-roboto';
 
 const theme = createMuiTheme({
+  typography: {
+    h1: {
+      fontSize: "2em",
+      fontWeight: "bold"
+    },
+    h2: {
+      fontSize: "1.2em"
+    }
+  },
   palette: {
     primary: {
       main: orange[500]
@@ -25,6 +34,17 @@ function App() {
 
       <div className="App">
         <header className="App-header">
+          <h1>Welcome to MUI</h1>
+          <Typography variant="h1">
+            Welcome to MUI
+          </Typography>
+
+          <Typography variant="h2" component="h1">
+            Welcome to MUI
+          </Typography>
+          <Typography variant="h4">
+            Learn how to use material-ui
+          </Typography>
           <Buttons />
           <Checkboxes />
           <Inputs />
