05eae43588695e63874678770435e12031df9e64
Facundo Cristaldo <facu.cristaldo.96@gmail.com>
2021-03-06 16:58:43 -0300

    material Button and Button Group

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+//MaterialUI Comopnents
+import Buttons from './components/Buttons'
 
 function App() {
+  const style = {
+    fontSize: "1.2em"
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <Buttons />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/components/Buttons.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+export default function Buttons() {
+    return (
+        <div>
+            <Button
+                variant="contained"
+                color="primary"
+                href="https://www.google.com"
+                startIcon={<SaveIcon />}
+            >
+                Button with icon
+        </Button>
+
+            <ButtonGroup>
+
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => alert("save")}
+                    startIcon={<SaveIcon />}
+                >
+                    First Button in Group
+        </Button>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => alert("delete")}
+                    startIcon={<DeleteIcon />}
+                >
+                    Second Button in Group
+          </Button>
+            </ButtonGroup>
+        </div>
+    )
+}
